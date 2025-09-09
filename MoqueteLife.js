879ed646ddecc76548e054fec613840e345055a5
Eron Moquete <eronmoquete4@gmail.com>
2025-09-09 17:35:12 +0800

    Added operators for daily routine

--- MoqueteLife.js
+++ MoqueteLife.js
@@ -5,3 +5,11 @@ const yearLevel = "2nd year";
 console.log("Name:", Name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
+
+const hoursSleep = 6;
+const hoursStudy = 5;
+const totalDailyHours = hoursSleep + hoursStudy;
+
+console.log("Hours of Sleep:", hoursSleep);
+console.log("Hours of Study:", hoursStudy);
+console.log("Total Daily Routine Hours:", totalDailyHours);
