51aba8ffa7b058a11c852fc941ff3bf3ca1005eb
Eron Moquete <eronmoquete4@gmail.com>
2025-09-09 17:33:17 +0800

    Added Student Variables

---
+++ MoqueteLife.js
@@ -0,0 +1,7 @@
+const Name = "Eron Moquete";
+const course = "Bachelor of Science in Information Technology";
+const yearLevel = "2nd year";
+
+console.log("Name:", Name);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
